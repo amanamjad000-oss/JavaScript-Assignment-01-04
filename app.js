312ce9624d3475d3e34781afaf044ac49be8e9e2
amanamjad000-oss <amanamjad000@gmail.com>
2026-01-09 10:43:28 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,115 @@
+              //Chapter 01
+
+//Chapter 01 (Q No 01)
+//alert("Welcome to my page");
+
+//Chapter 01 (Q No 02)
+//alert("Welcome to my page\n");
+
+//Chapter 01 (Q No 03)
+//alert("Learning JavaScript\nBest of luck!");
+
+//Chapter 01 (Q No 04)
+//alert("Learning JavaScript");
+//alert("Best of luck")
+
+//Chapter 01 (Q No 05)
+//console.log("Hello,Welcome back");
+
+               //**********//
+
+        //Chapter 02
+
+//Chapter 02 (Q No 01)  
+//var username;  
+
+//Chapter 02 (Q No 02)
+//var myName = "Pakeeza Noor";
+
+//Chapter 02 (Q No 03)
+//var message; //a
+//message = "Hello World"; //b
+//alert(message); //c
+
+//Chapter 02 (Q No 04)
+//var studentName ="Pakeeza Noor"
+//var age =15;
+//var course ="Computer Science";
+//alert("Student Name:" + studentName);
+//alert("Age:" + age);
+//alert("Course:" + course);
+
+//Chapter 02 (Q No 05)
+//var alertMessage ="Welcome to JavaScript!";
+//alert(alertMessage);
+
+//Chapter 02 (Q No 06)
+//var  email ="amanamjad000@gmail.com";
+//alert("My email address is" + email);
+
+//Chapter 02 (Q No 07)
+//var book ="A snarter way to learn JavaScript";
+//alert("Iam trying to learn from the book" +book);
+
+//Chapter 02 (Q No 08)
+//alert("I can write HTML content through JavaScript");
+
+//Chapter 02 (Q No 09)
+//var message2 = "I can insert HTML element with JavaScript";
+//alert(message2);
+//document.write("<h1> I can insert HTML element with JavaScript </h1>")
+
+               //***********//
+
+           //Chapter 03
+
+//Chapter 03 (Q No 01)
+//var age = 15;
+//alert("Iam" + age + "years old");
+//alert("you visited this site 1 time");
+
+//Chapter 03 (Q No 02)
+//var visitCount = 1;
+//document.write("You have visited this site" + visitCount +"time.<br>")
+
+//Chapter 03 (Q No 03)
+//var birthYear = 2010;
+//document.write("My birth year" + birthYear + "<br>");
+//document.write("Dta type of my declared variable is number<br>");
+
+//Chapter 03 (Q No 04)
+//var visiterName = "Pakeeza";
+//var productTitle ="Watch";
+//var quantity =5;
+//document.write(visiterName + "ordered" + quantity + "" +productTitle + "(s) on XYZ Clothing store");
+
+           //************//
+
+//Chapter 04 (Q No 01)
+ //var name,age,city;
+
+//Chapter 04 (Q No 02)
+//Legalvariablenames
+//var name,age,totalMarks,student__name,city1
+
+//iilegalvariable
+//1name
+// studentnames
+// total-marks
+// @City
+// class
+
+//Chapter 04 (Q No 03)
+//a)document.write("<hr><h1>Rule fir naming JS variables </h1><br></hr>");
+//b)document.write("Variables namescan only contain letters,numbers,$ and__.For example $my __ 1st variable <br><br>");
+//c)document.write("Variables must begin with a letter,$ or__. For example $name__, name or name<br><br>");
+//d)document.write("Variable names are case sensitive <br><br>");
+//e)document.write("Variable names should not be JS keywords");
+
+               //***********//
+
+//Chapter 05 (Q No 01)
+//var num1 = 8;
+//var num2 = 4;
+//var add num1 + num2;
+//document.write("Sum of" + num1 + "and" + num2 + "is" + add + "<br>");
